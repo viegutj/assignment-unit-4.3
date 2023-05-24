8c051db899e6e0a161e2266a12775283ddec86c2
Jonathan Viegut <viegutj@gmail.com>
2023-05-23 21:25:51 -0500

    Finished the required parts of assigment part 3. Could not find a way to console.log listItems() and spent way to long trying to figure it out. Comments written in the code.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,13 +2,35 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = []
 
+function addItem(item) {
+    basket.push(item)
+    return true
+}
+//testing addItem()
+console.log("Test - item has been added:", addItem("potato"), "Basket now has:", basket);
 
+function listItems() {
+    console.log("This is a list of the items in the basket:");
+    for (let i = 0; i < basket.length; i++) {
+        console.log(`${basket[i]}`);
+    }
+    
+}
 
+//Testing listItems()
+listItems()
+// NOTE: I cannot find a way to console.log listItems()
+// without printing an "undefined" at the end of my list.
+// I can add a return value of "" to eliminate the "undefined",
+// but then I get a blank line. Any advice?
 
+function empty () {
+    return basket = [];
+}
 
-
-
+console.log("This is an empty array:", empty());
 
 
 
