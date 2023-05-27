9b5ba66ce2c5bc6c9067d9642be1f1fb5ecb6d24
Jonathan Viegut <viegutj@gmail.com>
2023-05-27 12:11:17 -0500

    Updated stretch goals. Couldn't figure out 4. Will come back to it if time allows.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -32,7 +32,62 @@ function empty () {
 
 console.log("This is an empty array:", empty());
 
+//STETCH GOALS
 
+// 2. Add a global const named maxItems and set it to 5.
+
+// The code seems to think maxItems already exists?
+// I cannot declare it using const, or I will get an error
+// message "Variable declaration expected.ts(1134)".
+maxItems = 5
+
+
+// 3. Create a function called isFull(). It should:
+// return false if the basket contains less than max number of items
+// return true otherwise (equal or more than maxItems)
+function isFull() {
+    if (basket.length < maxItems) {
+        return false;
+    } else {
+        return true;
+    }
+}
+console.log("Is the basket full?", isFull());
+
+// 3. Update the required addItem function to:
+// Use the isFull function to prevent more than maxItems from being added to the basket.
+// If an item was added to the array, return true
+// If there was no room and the item could not be added return false
+
+function addItem(item) {
+    if (basket.length < 5) {
+        basket.push(item);
+        return true;
+    } else {
+        console.log('no room for new items');
+        return false;
+    }
+}
+console.log("here we add a new item", addItem("veggie"));
+
+4. // Create a function called removeItem. It should:
+// Take an input parameter for a string item
+// Use Array.indexOf to find the index of the first matching item in the basket.
+// Use Array.splice to remove the first matching item from the basket.
+// Return the item removed or null if the item was not found
+
+// Not sure what I'm doing wrong on this one. Would love to talk it over! :)
+
+function removeItem(item) {
+    console.log(basket.indexOf(item));
+    for (let i = 0; i < basket.length; i++) 
+    if (basket.length > -1) {
+        console.log(basket.splice(item));
+        return "item removed"
+    } else {
+        return "null"
+    }
+}
 
 
 // DO NOT MODIFY
